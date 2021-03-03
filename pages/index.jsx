823f1df5721bce6e980d797cd45d63653eed5f2e
To Huynh <huynh.nto@gmail.com>
2021-03-03 11:08:09 -0800

    Fix styling format

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head"
+import styles from "../styles/Home.module.css"
 
 export default function Home() {
   return (
@@ -9,9 +9,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          AnnoREP
-        </h1>
+        <h1 className={styles.title}>AnnoREP</h1>
       </main>
     </div>
   )
