51e83cd38050052bf54c82e398361965dc02d83b
To Huynh <37560480+tohuynh@users.noreply.github.com>
2021-05-04 15:37:35 -0700

    feature/ati-project-summary-view (#8)
    * Add ati project
    
    * Fix lint errors

---
+++ features/ati/AtiProject/AtiProject.stories.tsx
@@ -0,0 +1,18 @@
+import { Story, Meta } from "@storybook/react"
+
+import AtiProject, { AtiProjectProps } from "."
+
+export default {
+  component: AtiProject,
+  title: "ATI/ATI Project",
+} as Meta
+
+const Template: Story<AtiProjectProps> = (args) => <AtiProject {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+  id: "ati-project-id-1",
+  title: "ATI Project Title",
+  version: "1.0.0",
+  status: "Draft",
+}
---
+++ features/ati/AtiProject/index.tsx
@@ -0,0 +1,34 @@
+import React, { FC } from "react"
+
+import Link from "next/link"
+
+import "./styles.css"
+
+export interface AtiProjectProps {
+  /**The id of the ati project */
+  id: string
+  /**The title of the ati project */
+  title: string
+  /**The version of the ati project */
+  version: string
+  /**The status of the ati project */
+  status: string
+}
+
+/**A summary view of an ati project */
+const AtiProject: FC<AtiProjectProps> = ({ id, title, version, status }) => {
+  return (
+    <section aria-label={title} className="ar--ati-card">
+      <h2>
+        <Link href={`/ati/${id}`}>
+          <a className="bx--link bx--link--lg" href={`/ati/${id}`}>
+            {title}
+          </a>
+        </Link>
+      </h2>
+      <span className="ar--secondary-text">{`Version ${version} • ${status}`}</span>
+    </section>
+  )
+}
+
+export default AtiProject
