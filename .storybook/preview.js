f09d5a876a0bc7654e0a80a6acd18ddbd1a0d4f4
To Huynh <huynh.nto@gmail.com>
2021-05-04 18:47:45 -0700

    import carbon style

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,5 @@
 import "../styles/globals.css"
+import "carbon-components/css/carbon-components.css"
 
 export const parameters = {
   actions: { argTypesRegex: "^(on|handle)[A-Z].*" },
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app"
 import "../styles/globals.css"
+import "carbon-components/css/carbon-components.css"
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return <Component {...pageProps} />
