7b99a05735e31521018d8449dd8120daf4f4f880
To Huynh <huynh.nto@gmail.com>
2021-07-09 19:46:33 -0700

    Remove dataverse from login link

--- features/dataverse-auth/LoginLink/index.tsx
+++ features/dataverse-auth/LoginLink/index.tsx
@@ -9,7 +9,7 @@ const LoginLink: FC = () => {
   return (
     <Link href={DATAVERSE_LOGIN_PATH}>
       <a className="bx--link bx--link--lg" href={DATAVERSE_LOGIN_PATH}>
-        Log in (Dataverse)
+        Log in
       </a>
     </Link>
   )
