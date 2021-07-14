c547adb79772c2084ede2d87c12e6e202cf4921b
To Huynh <huynh.nto@gmail.com>
2021-07-13 22:34:54 -0700

    Add isloading

--- features/ati/NewAtiProjectForm/index.tsx
+++ features/ati/NewAtiProjectForm/index.tsx
@@ -1,8 +1,16 @@
-import React, { FC, FormEventHandler } from "react"
+import React, { FC, FormEventHandler, useState } from "react"
 
 import FormData from "form-data"
 import axios from "axios"
-import { Button, Form, FileUploader, Select, SelectItem, Link } from "carbon-components-react"
+import {
+  Button,
+  Form,
+  FileUploader,
+  Select,
+  SelectItem,
+  Link,
+  Loading,
+} from "carbon-components-react"
 import { useRouter } from "next/router"
 
 import { IDataset } from "../../../types/dataverse"
@@ -18,6 +26,7 @@ export interface NewAtiProjectFormProps {
 const NewAtiProjectForm: FC<NewAtiProjectFormProps> = ({ datasets, serverUrl }) => {
   const router = useRouter()
   //const [errorMsg, setErrorMsg] = useState<string>("")
+  const [isLoading, setIsLoading] = useState<boolean>(false)
   const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault()
     const target = e.target as typeof e.target & {
@@ -26,7 +35,7 @@ const NewAtiProjectForm: FC<NewAtiProjectFormProps> = ({ datasets, serverUrl }) 
     }
     const formData = new FormData()
     formData.append("manuscript", target.manuscript.files[0])
-    //loading start on the creat button
+    setIsLoading(true)
     Promise.all([
       axios({
         method: "PUT",
@@ -55,7 +64,7 @@ const NewAtiProjectForm: FC<NewAtiProjectFormProps> = ({ datasets, serverUrl }) 
       )
       .then(
         () => {
-          //loading over
+          setIsLoading(false)
           router.push(`/ati/${target.dataset.value}`)
         },
         (error) => {
@@ -64,64 +73,67 @@ const NewAtiProjectForm: FC<NewAtiProjectFormProps> = ({ datasets, serverUrl }) 
       )
       .catch((error) => {
         error
-        //loading over
+        setIsLoading(false)
         //toast the error
       })
   }
   //TODO: is ownerId=1 justified?
   return (
-    <div className="ar--form-container">
-      <Form encType="multipart/form-data" onSubmit={onSubmit}>
-        <h1 className="ar--form-title">
-          New <abbr>ATI</abbr> Project
-        </h1>
-        <p className="ar--form-desc">
-          Link to a Dataverse dataset and upload a manuscript to create a new <abbr>ATI</abbr>{" "}
-          project
-        </p>
-        <div className="ar--form-item">
-          <Link
-            href={`${serverUrl}/dataset.xhtml?ownerId=1`}
-            target="_blank"
-            rel="noopener noreferrer"
-            size="lg"
-          >
-            Create dataset
-          </Link>
-        </div>
-        <div className="ar--form-item">
-          <Select
-            name="dataset"
-            helperText="If your dataset is already stored in a Dataverse, choose a dataset to link to your ATI project."
-            id="dataverse-dataset"
-            labelText="Link to a Dataverse dataset"
-            required={true}
-            aria-required={true}
-          >
-            {datasets.map((dataset) => (
-              <SelectItem key={dataset.id} text={dataset.title} value={dataset.id} />
-            ))}
-          </Select>
-        </div>
-        <div className="ar--form-item">
-          <FileUploader
-            aria-required={true}
-            accept={[".docx, .pdf"]}
-            buttonKind="tertiary"
-            buttonLabel="Add file"
-            filenameStatus="edit"
-            iconDescription="Clear file"
-            labelDescription="Supported file types are .docx and .pdf."
-            labelTitle="Manuscript"
-            name="manuscript"
-            size="small"
-          />
-        </div>
-        <Button className="ar--form-submit-btn" type="submit">
-          Create project
-        </Button>
-      </Form>
-    </div>
+    <>
+      {isLoading && <Loading description="Creating ATI project" />}
+      <div className="ar--form-container">
+        <Form encType="multipart/form-data" onSubmit={onSubmit}>
+          <h1 className="ar--form-title">
+            New <abbr>ATI</abbr> Project
+          </h1>
+          <p className="ar--form-desc">
+            Link to a Dataverse dataset and upload a manuscript to create a new <abbr>ATI</abbr>{" "}
+            project
+          </p>
+          <div className="ar--form-item">
+            <Link
+              href={`${serverUrl}/dataset.xhtml?ownerId=1`}
+              target="_blank"
+              rel="noopener noreferrer"
+              size="lg"
+            >
+              Create dataset
+            </Link>
+          </div>
+          <div className="ar--form-item">
+            <Select
+              name="dataset"
+              helperText="If your dataset is already stored in a Dataverse, choose a dataset to link to your ATI project."
+              id="dataverse-dataset"
+              labelText="Link to a Dataverse dataset"
+              required={true}
+              aria-required={true}
+            >
+              {datasets.map((dataset) => (
+                <SelectItem key={dataset.id} text={dataset.title} value={dataset.id} />
+              ))}
+            </Select>
+          </div>
+          <div className="ar--form-item">
+            <FileUploader
+              aria-required={true}
+              accept={[".docx, .pdf"]}
+              buttonKind="tertiary"
+              buttonLabel="Add file"
+              filenameStatus="edit"
+              iconDescription="Clear file"
+              labelDescription="Supported file types are .docx and .pdf."
+              labelTitle="Manuscript"
+              name="manuscript"
+              size="small"
+            />
+          </div>
+          <Button className="ar--form-submit-btn" type="submit">
+            Create project
+          </Button>
+        </Form>
+      </div>
+    </>
   )
 }
 
