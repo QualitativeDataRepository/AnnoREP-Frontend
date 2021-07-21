72a9f9ea22c106f1d0e3b5cca9c4a55796ded521
To Huynh <huynh.nto@gmail.com>
2021-07-21 14:08:58 -0700

    Remove comment

--- features/ati/AtiExportAnnotations/index.tsx
+++ features/ati/AtiExportAnnotations/index.tsx
@@ -12,12 +12,7 @@ interface AtiExportAnnotationstProps {
   manuscript: IManuscript
   canExportAnnotations: boolean
 }
-//TODO: get arcore/source-manuscript-id/pdf, so user can upload it to publish destination
-//api call to get most recent annoations from hypo, using user hypo api token
-//api call to send annotations to new manuscription url, using qdr-user hypo api token
-//ui has to link to publish dataset
-//ui has download arcore/source-manuscript-id/pdf,
-//ui has input to enter new manuscript url
+
 const AtiExportAnnotations: FC<AtiExportAnnotationstProps> = ({
   manuscript,
   canExportAnnotations,
