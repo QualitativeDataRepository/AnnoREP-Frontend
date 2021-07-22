0b9827b961f23e6b82a12100e624a42577651d41
To Huynh <huynh.nto@gmail.com>
2021-07-22 13:38:43 -0700

    Remove fluentui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,6 @@ import { Provider } from "next-auth/client"
 import "../styles/globals.css"
 import "carbon-components/css/carbon-components.min.css"
 
-import { initializeIcons } from "@fluentui/react"
-initializeIcons()
-
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <Provider session={pageProps.session}>
