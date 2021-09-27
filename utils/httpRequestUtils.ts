6ae4932cda1b444177bad186435264a53b7a556e
To Huynh <huynh.nto@gmail.com>
2021-09-26 20:16:33 -0700

    Fix unknown error

--- utils/httpRequestUtils.ts
+++ utils/httpRequestUtils.ts
@@ -1,11 +1,9 @@
-import axios, { AxiosError } from "axios"
+import axios from "axios"
 import { REQUEST_DESC_HEADER_NAME } from "../constants/http"
 import { AnnoRepResponse } from "../types/http"
 
-export const getResponseFromError = (
-  e: Error | AxiosError,
-  requestDesc?: string
-): AnnoRepResponse => {
+export const getResponseFromError = (e: unknown, requestDesc?: string): AnnoRepResponse => {
+  const annoRepResposne: AnnoRepResponse = { status: 400, message: "" }
   if (axios.isAxiosError(e)) {
     const requestInfo =
       e.config.headers[REQUEST_DESC_HEADER_NAME] ||
@@ -19,23 +17,27 @@ export const getResponseFromError = (
         `HTTP ${e.response.status} ${e.response.statusText}`
       failureMessage = `${additional}. ${failureMessage}`
     }
-    return {
-      status: e.response?.status || 400,
-      message: failureMessage,
-    }
+    annoRepResposne.status = e.response?.status || 400
+    annoRepResposne.message = failureMessage
+  } else if (e instanceof Error) {
+    annoRepResposne.status = 400
+    annoRepResposne.message = `${e.message}${requestDesc ? ` ${requestDesc} failed.` : ""}`
   } else {
-    return {
-      status: 400,
-      message: `${e.message}${requestDesc ? ` ${requestDesc} failed.` : ""}`,
-    }
+    annoRepResposne.status = 400
+    annoRepResposne.message = `Unknown error ${e}`
   }
+  return annoRepResposne
 }
 
-export const getMessageFromError = (e: Error | AxiosError): string => {
+export const getMessageFromError = (e: unknown): string => {
+  let msg: string
   if (axios.isAxiosError(e)) {
     //All internal api endpoints returns a body with message field.
-    return e.response?.data.message
+    msg = e.response?.data.message
+  } else if (e instanceof Error) {
+    msg = e.message
   } else {
-    return e.message
+    msg = `${e}`
   }
+  return msg
 }
