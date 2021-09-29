cb44d2f09c774558cd2a64b40e0b157495813ed2
To Huynh <huynh.nto@gmail.com>
2021-09-29 13:27:20 -0700

    Test range in arrayUtils

--- utils/arrayUtils.ts
+++ utils/arrayUtils.ts
@@ -1,5 +1,9 @@
-//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from#sequence_generator_range
-//Sequence generator
-//Array includes stop
+/**
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from#sequence_generator_range
+ * @param start start > 0
+ * @param stop stop >= start
+ * @param step step > 0
+ * @returns An array of numbers from start to stop, inclusive and incremented by step.
+ */
 export const range = (start: number, stop: number, step: number) =>
   Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + i * step)
