800ae34088627232d517795b87d5b3d0149525de
To Huynh <huynh.nto@gmail.com>
2021-11-10 17:25:10 -0800

    Save user's choice to local storage

--- features/auth/HypothesisLoginNotificaton/index.tsx
+++ features/auth/HypothesisLoginNotificaton/index.tsx
@@ -1,18 +1,38 @@
-import { FC } from "react"
+import { FC, useState, useEffect } from "react"
 
 import { InlineNotification, NotificationActionButton } from "carbon-components-react"
 
+const HYPOTHESIS_LOGIN_NOTIFICATION_IS_VISIBLE = "hypotheis-login-notification-is-visible"
+
 const HypothesisLoginNotification: FC = () => {
-  return (
-    <InlineNotification
-      hideCloseButton
-      lowContrast
-      kind="info"
-      statusIconDescription="info"
-      title="Log in to Hypothes.is to see your annotations in the Annotation sidebar."
-      actions={<NotificationActionButton>OK</NotificationActionButton>}
-    />
-  )
+  const [notificationIsVisible, setNotificationIsVisible] = useState(false)
+
+  const onClickOK = () => {
+    localStorage.setItem(HYPOTHESIS_LOGIN_NOTIFICATION_IS_VISIBLE, "false")
+    setNotificationIsVisible(false)
+  }
+
+  useEffect(() => {
+    const isVisible = localStorage.getItem(HYPOTHESIS_LOGIN_NOTIFICATION_IS_VISIBLE)
+    if (isVisible === null || isVisible === "true") {
+      setNotificationIsVisible(true)
+    }
+  }, [])
+
+  if (notificationIsVisible) {
+    return (
+      <InlineNotification
+        hideCloseButton
+        lowContrast
+        kind="info"
+        statusIconDescription="info"
+        title="Log in to Hypothes.is to see your annotations in the Annotation sidebar."
+        actions={<NotificationActionButton onClick={onClickOK}>OK</NotificationActionButton>}
+      />
+    )
+  }
+
+  return null
 }
 
 export default HypothesisLoginNotification
