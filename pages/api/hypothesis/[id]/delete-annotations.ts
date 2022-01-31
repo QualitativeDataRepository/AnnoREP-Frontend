be5d14226c8352bc6a45461ad8c8f7080df7be00
To Huynh <huynh.nto@gmail.com>
2022-01-31 10:59:53 -0800

    delete-annotations endpoint can only delete deletable annotations

--- pages/api/hypothesis/[id]/delete-annotations.ts
+++ pages/api/hypothesis/[id]/delete-annotations.ts
@@ -8,15 +8,26 @@ import { getResponseFromError } from "../../../../utils/httpRequestUtils"
 export default async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
   if (req.method === "DELETE") {
     const session = await getSession({ req })
+    const { data } = await axios.get(`${process.env.HYPOTHESIS_SERVER_URL}/api/profile`, {
+      headers: { Authorization: `Bearer ${process.env.ADMIN_HYPOTHESIS_API_TOKEN}` },
+    })
     if (session) {
       const { id, isAdminAuthor } = req.query
       const { annotations } = req.body
       const requestDesc = `Deleting annotations from dataset ${id}`
-      const { hypothesisApiToken: userHypothesisApiToken } = session
-      const hypothesisApiToken =
-        isAdminAuthor === "true" ? process.env.ADMIN_HYPOTHESIS_API_TOKEN : userHypothesisApiToken
+      const { hypothesisApiToken: userHypothesisApiToken, hypothesisUserId: userHypothesisUserId } =
+        session
+      let hypothesisApiToken = userHypothesisApiToken
+      let hypothesisUserId = userHypothesisUserId
+      if (isAdminAuthor === "true") {
+        hypothesisApiToken = process.env.ADMIN_HYPOTHESIS_API_TOKEN
+        hypothesisUserId = data.userid
+      }
       try {
-        const deleteAnns: AxiosPromise<any>[] = annotations.map((annotation: any) => {
+        const deletableAnnotations = annotations.filter((annotation: any) =>
+          annotation.permissions.delete.includes(hypothesisUserId)
+        )
+        const deleteAnns: AxiosPromise<any>[] = deletableAnnotations.map((annotation: any) => {
           return axios.delete(
             `${process.env.HYPOTHESIS_SERVER_URL}/api/annotations/${annotation.id}`,
             {
@@ -29,7 +40,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         })
         await Promise.all(deleteAnns)
         res.status(200).json({
-          totalDeleted: annotations.length,
+          totalDeleted:
+            deletableAnnotations.length === annotations.length
+              ? annotations.length
+              : `${deletableAnnotations.length}/${annotations.length}`,
         })
       } catch (e) {
         const { status, message } = getResponseFromError(e, requestDesc)
