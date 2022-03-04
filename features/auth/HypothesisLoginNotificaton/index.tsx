9802a915c99aefa66a4622359e178c4e8c29e345
To Huynh <huynh.nto@gmail.com>
2022-03-04 14:06:45 -0800

    Upgrade banning to `warning`

--- features/auth/HypothesisLoginNotificaton/index.tsx
+++ features/auth/HypothesisLoginNotificaton/index.tsx
@@ -24,9 +24,9 @@ const HypothesisLoginNotification: FC = () => {
       <InlineNotification
         hideCloseButton
         lowContrast
-        kind="info"
-        statusIconDescription="info"
-        title="Log in to Hypothes.is to see your annotations in the Annotation sidebar."
+        kind="warning-alt"
+        statusIconDescription="warning"
+        title="Log in to Hypothes.is to see your annotations in the Annotation sidebar on the right."
         actions={<NotificationActionButton onClick={onClickOK}>OK</NotificationActionButton>}
       />
     )
