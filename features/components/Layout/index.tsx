13b09b493f1a590b7b104070fc085edd7d10dd78
To Huynh <huynh.nto@gmail.com>
2022-03-16 20:07:41 -0700

    Fix path

--- features/components/Layout/index.tsx
+++ features/components/Layout/index.tsx
@@ -38,7 +38,7 @@ const Layout: FC<LayoutProps> = ({ title, children, isLoggedIn, isFullWidth, has
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
