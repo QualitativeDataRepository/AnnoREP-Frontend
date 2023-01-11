cd2762fee03962cc3085efa71ddd7675e3db3a63
To Huynh <huynh.nto@gmail.com>
2023-01-10 17:16:48 -0800

    Prelimary matomo test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
+import { useEffect } from "react"
 import type { AppProps } from "next/app"
 import { Provider } from "next-auth/client"
+
+import init from "../features/analytics/matomo"
 
 import "../styles/globals.css"
 import "carbon-components/css/carbon-components.min.css"
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  useEffect(() => {
+    init({ url: "https://tnhar.matomo.cloud/", siteId: "1" })
+  }, [])
   return (
     <Provider session={pageProps.session}>
       <Component {...pageProps} />
