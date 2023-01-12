5acd167bd20eb37b9e9a4258b12c36c2186c3833
To Huynh <37560480+tohuynh@users.noreply.github.com>
2023-01-12 14:20:54 -0800

    feature/add-matomo-analytics (#106)
    * Prelimary matomo test
    
    * Clean matomo feature
    
    * Remove slash
    
    * Use setCustomUrl correctly
    
    * Enable tracking JSErrors

---
+++ features/analytics/matomo.ts
@@ -0,0 +1,79 @@
+import { default as Router } from "next/router"
+
+declare global {
+  interface Window {
+    _paq: string[][]
+  }
+}
+
+interface InitSettings {
+  url: string
+  siteId: string
+  jsTrackerFile?: string
+  phpTrackerFile?: string
+}
+
+function push(args: string[]): void {
+  if (!window._paq) {
+    window._paq = []
+  }
+  window._paq.push(args)
+}
+
+export function init({
+  url,
+  siteId,
+  jsTrackerFile = "matomo.js",
+  phpTrackerFile = "matomo.php",
+}: InitSettings): void {
+  window._paq = window._paq !== null ? window._paq : []
+  if (!url) {
+    console.warn("Matomo disabled, please provide matomo url")
+    return
+  }
+  if (!siteId) {
+    console.warn("Matomo disabled, please provide site id")
+    return
+  }
+
+  push(["trackPageView"])
+  push(["enableLinkTracking"])
+  push(["enableJSErrorTracking"])
+  push(["setTrackerUrl", `${url}/${phpTrackerFile}`])
+  push(["setSiteId", siteId])
+
+  const scriptElement = document.createElement("script")
+  const refElement = document.getElementsByTagName("script")[0]
+  scriptElement.type = "text/javascript"
+  scriptElement.async = true
+  scriptElement.src = `${url}/${jsTrackerFile}`
+  if (refElement.parentNode) {
+    refElement.parentNode.insertBefore(scriptElement, refElement)
+  }
+
+  //start with location.origin + location.pathname
+  //when user navigate the site, use Router.pathname to get the next url
+  let previousUrl = `${location.origin}${location.pathname}`
+
+  Router.events.on("routeChangeStart", (path: string): void => {
+    //just the pathname
+    const [pathname] = path.split("?")
+
+    if (previousUrl) {
+      push(["setReferrerUrl", previousUrl])
+    }
+    const nextUrl = `${location.origin}${pathname}`
+    push(["setCustomUrl", nextUrl])
+    push(["deleteCustomVariables", "page"])
+    previousUrl = nextUrl
+  })
+
+  Router.events.on("routeChangeComplete", (): void => {
+    // In order to ensure that the page title had been updated,
+    // we delayed pushing the tracking to the next tick.
+    setTimeout(() => {
+      push(["setDocumentTitle", document.title])
+      push(["trackPageView"])
+    }, 0)
+  })
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,19 @@
+import { useEffect } from "react"
 import type { AppProps } from "next/app"
 import { Provider } from "next-auth/client"
+
+import { init } from "../features/analytics/matomo"
 
 import "../styles/globals.css"
 import "carbon-components/css/carbon-components.min.css"
 
+const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL
+const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID
+
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  useEffect(() => {
+    init({ url: MATOMO_URL as string, siteId: MATOMO_SITE_ID as string })
+  }, [])
   return (
     <Provider session={pageProps.session}>
       <Component {...pageProps} />
