4e325aa810004ca3bece4b72ae1d912c41ac60ab
To Huynh <37560480+tohuynh@users.noreply.github.com>
2024-04-02 16:57:39 -0700

    code-quality/Incomplete-URL-substring-sanitization (#170)
    * Use hostname to check if the retry is for hypothesis api
    
    * Use getMessageFromError

--- features/app/index.ts
+++ features/app/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios"
 import axiosRetry from "axios-retry"
+import { isSameHostName } from "../../utils/httpRequestUtils"
 
 const HYPOTHESIS_API_BASE_URL = process.env.HYPOTHESIS_SERVER_URL || "https://api.hypothes.is"
 
@@ -21,16 +22,22 @@ axiosRetry(axiosClient, {
           return delay
         }
       }
-      if (error.response.status === 429 && error.config?.url?.includes(HYPOTHESIS_API_BASE_URL)) {
-        return retryCount * 2000 /** ms */
+      if (error.response.status === 429 && error.config?.url) {
+        const isHypothesisUrl = isSameHostName(error.config.url, HYPOTHESIS_API_BASE_URL)
+        if (isHypothesisUrl) {
+          return retryCount * 2000 /** ms */
+        }
       }
     }
     return axiosRetry.exponentialDelay(retryCount)
   },
   retryCondition: (e) => {
     if (e.response) {
-      if (e.response.status === 429 && e.response.config.url?.includes(HYPOTHESIS_API_BASE_URL)) {
-        return true
+      if (e.response.status === 429 && e.response.config.url) {
+        const isHypothesisUrl = isSameHostName(e.response.config.url, HYPOTHESIS_API_BASE_URL)
+        if (isHypothesisUrl) {
+          return true
+        }
       }
     }
     return axiosRetry.isNetworkOrIdempotentRequestError(e)
